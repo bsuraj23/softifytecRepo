a530853db59b63b4471624efb0c0c02419c73275
bsuraj231 <93305301+bsuraj231@users.noreply.github.com>
2022-04-07 07:02:06 +0530

    adding folders

---
+++ src/hello.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export default function hello() {
+  // Declare a new state variable, which we'll call "count"
+
+
+  return (
+    <div>
+     <>Hellow</>
+    </div>
+  );
+}
+
+    	
